e8e098eb58b9f0d9ac9fa20c9c6dea849e3e0aa2
Cassie Shum <cassiend@gmail.com>
2012-01-19 01:18:41 -0500

    adding more parameters around UI

--- src/com/spike/NeatWebView.java
+++ src/com/spike/NeatWebView.java
@@ -1,11 +1,15 @@
 package com.spike;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -27,50 +31,80 @@ import java.util.Map;
 
 public class NeatWebView extends Activity
 {
-//    WebView webView;
-    TokenCredentials tokenCredentials;
+    WebView webView;
+    final Activity activity = this;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.main);
-        WebView webView = new WebView(this);
-//        webView = (WebView) this.findViewById(R.id.webview);
+        setContentView(R.layout.main);
+        webView = (WebView) findViewById(R.id.webview);
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setCacheMode(WebSettings.LOAD_NORMAL);
-        webView.getSettings().setSupportZoom(true);
+
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.FAR);
 
         webView.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
+                if (url.startsWith("tel:")) {
+                    Intent intent = new Intent(Intent.ACTION_DIAL,
+                            Uri.parse(url));
+                    startActivity(intent);
+                }else if(url.startsWith("mailto:")){
+                    Intent intent = new Intent(Intent.ACTION_SENDTO,
+                            Uri.parse(url));
+                    startActivity(intent);
+                }else if(url.startsWith("http:") || url.startsWith("https:")) {
+                    view.loadUrl(url);
+                }
                 return true;
             }
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+                Toast.makeText(activity, "Error Code: " + Integer.toString(errorCode) + ": " + description, Toast.LENGTH_LONG).show();
+                view.loadUrl("http://www.google.com");
+            }
         });
-        setContentView(webView);
-        login();
+
         Map<String,String> map = new HashMap<String, String>();
-        map.put("Authorization", "OAuth " + tokenCredentials.current());
+        Log.d("Login", login().current());
+        map.put("Authorization", "OAuth " + login().current());
         map.put("X-Neat-Device", "android");
-        map.put("X-Neat-Version","1.0 build 183");
-        map.put("Accept", "text/html");
+//        map.put("X-Neat-Version","1.0 build 183");
+//        map.put("Accept", "text/html");
 
         webView.loadUrl("https://cloud-qa.neat.com/folders", map);
-//        webView.loadUrl("http://192.168.50.237:3000/folders", map);
+//        webView.loadUrl("http://192.168.1.10:3000/folders", map);
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.d("Back", "I'm here");
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            Toast.makeText(activity, "I just pushed the back button and now I'm catching the action!", Toast.LENGTH_LONG).show();
+            webView.loadUrl("http://www.google.com");
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
-    public void login() {
+    public TokenCredentials login() {
         String loginResponse = null;
+        TokenCredentials tokenCredentials = null;
         HttpClient httpClient = new DefaultHttpClient();
         HttpPost httpPost = new HttpPost("https://cloud-qa.neat.com/oauth/access_token");
-//        HttpPost httpPost = new HttpPost("http://192.168.50.237:3000/oauth/access_token");
+//        HttpPost httpPost = new HttpPost("http://192.168.1.10:3000/oauth/access_token");
 
         try {
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("client_secret", "ecd1a4efede18c7323018a20bb95f4acf828b2febcd22b759a6bc2bf858d4a44"));
-            nameValuePairs.add(new BasicNameValuePair("client_id", "4e9e5dd422b7a94bb7000108"));
-//            nameValuePairs.add(new BasicNameValuePair("client_secret", "91f85595c7d364294aa596f4378ad99fa587d9c636e6ce3b53790f5fe72e3985"));
-//            nameValuePairs.add(new BasicNameValuePair("client_id", "4e9e5dd422b7a94bb7000107"));
+//            nameValuePairs.add(new BasicNameValuePair("client_secret", "ecd1a4efede18c7323018a20bb95f4acf828b2febcd22b759a6bc2bf858d4a44"));
+//            nameValuePairs.add(new BasicNameValuePair("client_id", "4e9e5dd422b7a94bb7000108"));
+            nameValuePairs.add(new BasicNameValuePair("client_secret", "91f85595c7d364294aa596f4378ad99fa587d9c636e6ce3b53790f5fe72e3985"));
+            nameValuePairs.add(new BasicNameValuePair("client_id", "4e9e5dd422b7a94bb7000107"));
             nameValuePairs.add(new BasicNameValuePair("username", "sp.evan@mailinator.com"));
             nameValuePairs.add(new BasicNameValuePair("password", "p@ssw0rd"));
             nameValuePairs.add(new BasicNameValuePair("grant_type", "password"));
@@ -96,5 +130,6 @@ public class NeatWebView extends Activity
         } catch (JSONException e) {
             Log.e("Error", "JSON Exception");
         }
+        return tokenCredentials;
     }
 }
